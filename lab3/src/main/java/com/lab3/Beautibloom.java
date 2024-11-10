ef0126b6157fa1353cbbf7a5bc195befa39b6daa
EsahQ <kovalevvanc@gmail.com>
2024-11-10 20:37:40 +0300

    Добавлена лаб 3

---
+++ lab3/src/main/java/com/lab3/Beautibloom.java
@@ -0,0 +1,8 @@
+package com.lab3;
+public class Beautibloom extends Plant {
+    double bloom;
+    public Beautibloom(String name, double height, int old, double bloom) {
+        super(name, height, old);
+        this.bloom = bloom;
+    }
+}
---
+++ lab3/src/main/java/com/lab3/Plant.java
@@ -0,0 +1,11 @@
+package com.lab3;
+public class Plant {
+    String name;
+    double height;
+    int old;
+    public Plant(String name, double height, int old) {
+        this.name = name;
+        this.height = height;
+        this.old = old;
+    }
+}
